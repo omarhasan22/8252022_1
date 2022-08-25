edf83dec9d3e0c7d554b4a3d84095fb7b6c21c67
your <omar.hasan3894@gmail.com>
2022-08-25 11:12:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+fetch('https://training202103069-00.herokuapp.com/data')
+  .then((response) => response.json())
+  .then(json =>{
+    Student = document.getElementById('S')
+    json.Sheet1.forEach(e => {
+        
+        var option = document.createElement('option')
+        option.innerHTML=e['A'];
+        Student.appendChild(option);
+    });
+
+  });
