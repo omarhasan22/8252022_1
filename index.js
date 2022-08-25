8e6f8396585248c10d038e251928294dad1c164c
your <omar.hasan3894@gmail.com>
2022-08-25 14:02:34 +0300

    AAAA

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 fetch('https://training202103069-00.herokuapp.com/data')
   .then((response) => response.json())
   .then(json =>{
-    Student = document.getElementById('S')
-
-    image = document.getElementById('image')
-    img = document.createElement('img')
-    img.src='./A.jpg';
-    image.appendChild(img)
-    
+    Student = document.getElementById('S')    
     json.Sheet1.forEach(e => {
         
         var option = document.createElement('option')
